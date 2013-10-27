9bf11910b7f110b6a034e7731c3bd28b32f2c382
Ilkka Hakkarainen <ilkka.hakkarainen@cs.helsinki.fi>
2013-10-27 11:47:12 +0200

    Add secret token
    Heroku was bitching about it.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ratebeer::Application.config.secret_token = '24fd33552677f9f504773c18f4a3842cc7711a067e02f5b6cf64254ef5b5687e658dc5bdfe9e41c8e52fd1c0245cd494b10f3708bf7e9650452c9d4080704071'
